706992c2e4d25d6a8cbfd63c1ca88b28af002987
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-03-14 13:25:47 +0600

    Add files via upload

---
+++ SMPWOW - Wow.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<"W";
+    for(int i=0; i<x; i++)
+        cout<<"o";
+    cout<<"w"<<endl;
+
+    return 0;
+}
